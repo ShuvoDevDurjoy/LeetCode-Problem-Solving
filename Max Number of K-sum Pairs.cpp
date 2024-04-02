a952d64b882f135321a88b16c07cdd2e2dfb2111
Shuvo Dev Durjoy <shuvo.dev@northsouth.edu>
2024-04-02 22:50:37 +0600

    Create Max Number of K-sum Pairs.cpp

---
+++ Max Number of K-sum Pairs.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxOperations(vector<int>& nums, int k) {
+        int left = 0 ; 
+        int right = nums.size()-1 ; 
+        int found = 0 ; 
+        sort(nums.begin() , nums.end());
+        while(left<right){
+            if(nums[left]+nums[right]==k) {found++ ; left++ ; 
+            right-- ; }
+            else if(nums[left]+nums[right] < k){
+                left++ ; 
+            }
+            else {
+                right-- ; 
+            }
+        }
+        return found ; 
+    }
+};
