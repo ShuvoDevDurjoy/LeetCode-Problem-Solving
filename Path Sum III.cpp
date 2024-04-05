82c5befb7918fb6946199be37afe6cc671af46a5
Shuvo Dev Durjoy <shuvo.dev@northsouth.edu>
2024-04-05 23:14:04 +0600

    Create Path Sum III.cpp

---
+++ Path Sum III.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+int pathSum(TreeNode* root, int targetSum) {
+        if (!root) return 0;
+        return countPaths(root, targetSum) + pathSum(root->left, targetSum) + pathSum(root->right, targetSum);
+    }
+    
+    int countPaths(TreeNode* node, long int targetSum) {
+        if (!node) return 0;
+        long long int paths = (node->val == targetSum) ? 1 : 0;
+        paths += countPaths(node->left, targetSum - node->val);
+        paths += countPaths(node->right, targetSum - node->val);
+        return paths;
+    }
+};
