fbe1117a3fcc709e1d3e7f0e28ce51fdccee7eaa
Shuvo Dev Durjoy <shuvo.dev@northsouth.edu>
2024-10-17 13:42:10 +0600

    Create Monthly Transactions I.sql

---
+++ Monthly Transactions I.sql
@@ -0,0 +1,1 @@
+SELECT DATE_FORMAT(trans_date,'%Y-%m') as month, country,count(trans_date) as trans_count, COUNT(IF(state='approved',1,null)) as approved_count, SUM(amount) as trans_total_amount, SUM(IF(state='approved',amount,0)) as approved_total_amount from Transactions group by DATE_FORMAT(trans_date,'%Y-%m'),country;
